273b8a2f5a30effdb2a5ac6494945ae8f6092bf7
Stanislav <58264863+Chebannich@users.noreply.github.com>
2024-10-19 18:05:23 +0200

    refactor: change comments

--- src/index.js
+++ src/index.js
@@ -1,67 +1,73 @@
 module.exports = function toReadable(number) {
-  const singleNumbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-  const teenNumbers = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
-  const roundNumbers = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
-  const hundred = 'hundred'
+  // Arrays containing English words for single, teen, and round numbers
+  const singleNumbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const teenNumbers = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const roundNumbers = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const hundred = 'hundred'; // Word for hundred
+
+  // Split the number into an array of digits
   const arrayNum = ('' + number).split('');
 
-  if (arrayNum.length === 1) {  // if 0 <= number <= 9
-    return singleNumbers[arrayNum[0]]
+  // If the number is a single digit (0-9)
+  if (arrayNum.length === 1) {
+    return singleNumbers[arrayNum[0]]; // Return the corresponding word from singleNumbers
   }
 
-  if (arrayNum.length === 2) {  // if 10 <= number <= 99
+  // If the number is two digits (10-99)
+  if (arrayNum.length === 2) {
 
-    // if 10 <= number <= 19
+    // For numbers between 10 and 19
     if (number < 20) {
-      return teenNumbers[number - 10]
+      return teenNumbers[number - 10]; // Return the corresponding teen number
     }
-    // if 20 <= number <= 99
+    // For numbers between 20 and 99
     else if (number >= 20) {
 
-      // if second digit is 0
+      // If the second digit is 0 (e.g., 20, 30, 40, etc.)
       if (arrayNum[1] === '0') {
-        return roundNumbers[(arrayNum[0] - 2)]
+        return roundNumbers[(arrayNum[0] - 2)]; // Return the corresponding round number
       }
 
-      // if 1 <= second digit <= 9
+      // If the second digit is not 0 (e.g., 21, 35, 49, etc.)
       else {
-        return roundNumbers[(arrayNum[0] - 2)] + ' ' + singleNumbers[arrayNum[1]]
+        return roundNumbers[(arrayNum[0] - 2)] + ' ' + singleNumbers[arrayNum[1]]; // Combine the round number and single digit
       }
     }
   }
 
-  if (arrayNum.length === 3) {  // if 100 <= number <= 999
+  // If the number is three digits (100-999)
+  if (arrayNum.length === 3) {
 
-    // if second digit is 0
+    // If the second digit is 0
     if (arrayNum[1] === '0') {
 
-      // if third digit is 0
+      // If the third digit is also 0 (e.g., 100, 200, 300, etc.)
       if (arrayNum[2] === '0') {
-        return singleNumbers[(arrayNum[0])] + ' ' + hundred
+        return singleNumbers[(arrayNum[0])] + ' ' + hundred; // Return "X hundred"
       }
 
-      // if 1 <= third digit <= 9
+      // If the third digit is not 0 (e.g., 101, 206, 309, etc.)
       else {
-        return singleNumbers[(arrayNum[0])] + ' ' + hundred + ' ' + singleNumbers[arrayNum[2]]
+        return singleNumbers[(arrayNum[0])] + ' ' + hundred + ' ' + singleNumbers[arrayNum[2]]; // Return "X hundred Y"
       }
     }
 
-    // if second digit is 1
+    // If the second digit is 1 (e.g., 110, 215, 319, etc.)
     else if (arrayNum[1] === '1') {
-      return singleNumbers[(arrayNum[0])] + ' ' + hundred + ' ' + teenNumbers[arrayNum[2]]
+      return singleNumbers[(arrayNum[0])] + ' ' + hundred + ' ' + teenNumbers[arrayNum[2]]; // Return "X hundred teen number"
     }
 
-    // if second digit is more than 1
+    // If the second digit is greater than 1 (e.g., 120, 235, 349, etc.)
     else {
 
-      // if third digit is 0
+      // If the third digit is 0 (e.g., 120, 230, 340, etc.)
       if (arrayNum[2] === '0') {
-        return singleNumbers[(arrayNum[0])] + ' ' + hundred + ' ' + roundNumbers[(arrayNum[1] - 2)]
+        return singleNumbers[(arrayNum[0])] + ' ' + hundred + ' ' + roundNumbers[(arrayNum[1] - 2)]; // Return "X hundred round number"
       }
 
-      // if 1 <= third digit <= 9
+      // If the third digit is not 0 (e.g., 121, 235, 349, etc.)
       else {
-        return singleNumbers[(arrayNum[0])] + ' ' + hundred + ' ' + roundNumbers[(arrayNum[1] - 2)] + ' ' + singleNumbers[arrayNum[2]]
+        return singleNumbers[(arrayNum[0])] + ' ' + hundred + ' ' + roundNumbers[(arrayNum[1] - 2)] + ' ' + singleNumbers[arrayNum[2]]; // Return "X hundred round number Y"
       }
     }
   }
